ca671a02a71ddfce1b72845f01425c2c74d4f47c
Scott Gibson <scott.gibson@cqumail.com>
2018-02-22 18:17:19 +1000

    Added port and IP arguments.

--- register.py
+++ register.py
@@ -1,7 +1,20 @@
 import json
 import requests
 
-transaction = {"nodes": ["http://180.150.12.207:5000"]}
+from argparse import ArgumentParser
+
+parser = ArgumentParser()
+
+parser.add_argument('-i', '--ip', default='127.0.0.1', type=str, help='IP address of my node.')
+parser.add_argument('-p', '--port', default=5001, type=int, help='Port my node will listen on.')
+
+args = parser.parse_args()
+ip = args.ip
+port = args.port
+
+node = 'http://' + ip + ':' + str(port)
+
+transaction = {"nodes": [node]}
 url = 'http://159.89.89.46:5000/nodes/register'
 headers = {'Content-Type': 'application/json'}
 
