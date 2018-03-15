074cd6b93f9c156bba59a49bd755b15203e3e628
Scott Gibson <scott.gibson@cqumail.com>
2018-03-15 11:52:17 +1000

    Added random bin number generator.

--- trans.py
+++ trans.py
@@ -3,6 +3,8 @@ import requests
 
 from argparse import ArgumentParser
 
+from random import randint
+
 parser = ArgumentParser()
 
 parser.add_argument('-r', '--recip', default='127.0.0.1', type=str, help='IP address of the recipient.')
@@ -12,8 +14,19 @@ args = parser.parse_args()
 recip = args.recip
 port = args.port
 
+def new_bin(self):
+    """
+    Creates a random 4-digit bin number (bin_num)
+    """
+    range_start = 10**3
+    range_end = (10**4)-1
+    return randint(range_start, range_end)
+
+#Generate a random bin number.
+bin_num = new_bin(self)
+
 transaction = {"sender": "Scott", "recipient": "Digi Node",
- "bin_num": "1234"}
+         "bin_num": str(bin_num)}
 url = ('http://' + recip + ':' + str(port) + '/transactions/new')
 headers = {'Content-Type': 'application/json'}
 
